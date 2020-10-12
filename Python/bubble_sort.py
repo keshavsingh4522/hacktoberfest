f704dd494bd02932dd2b06e6a7b303b0b3b391ec
FrozenFighter <9080908+FrozenFighter@users.noreply.github.com>
2020-10-12 22:58:16 +0700

    Fix Redefining built-in 'list'

--- Python/bubble_sort.py
+++ Python/bubble_sort.py
@@ -1,14 +1,14 @@
 #defined function
-def bubble_sort(list):
+def bubble_sort(input_list):
   #get list length
-  count = len(list)
+  count = len(input_list)
 
   #Loop through an array
   for i in range(count):
     for j in range(0, count - i - 1):
       #Switch if greater value is found
-      if list[j] > list[j + 1]:
-        list[j], list[j + 1] = list[j + 1], list[j]
+      if input_list[j] > input_list[j + 1]:
+        input_list[j], input_list[j + 1] = input_list[j + 1], input_list[j]
 
 #Test simple
 example_list = [71, 77, 9, 11, 88]
@@ -16,4 +16,3 @@ example_list = [71, 77, 9, 11, 88]
 #call function
 bubble_sort(example_list)
 print(example_list)
-#OUTPUT: [9, 11, 71, 77, 88]
