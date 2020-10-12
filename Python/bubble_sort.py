0dec29c4c20301059b545a867fe837ff44faf9c9
FrozenFighter <9080908+FrozenFighter@users.noreply.github.com>
2020-10-12 22:52:04 +0700

    Bubble sort in python
    Example of bubble sort implement in python

---
+++ Python/bubble_sort.py
@@ -0,0 +1,18 @@
+#defined function
+def bubble_sort(list):
+  #get list length
+  count = len(list)
+
+  #Loop through an array
+  for i in range(count):
+    for j in range(0, count - i - 1):
+      #Switch if greater value is found
+      if list[j] > list[j + 1]:
+        list[j], list[j + 1] = list[j + 1], list[j]
+
+#Test simple
+list = [71, 77, 9, 11, 88]
+
+#call function
+bubble_sort(list)
+print(list)
