df6b96956d67da5ba16fa925f6f68caaa5912cc8
Ananya Sharma <Ananya Sharma>
2020-10-16 23:58:44 +0530

    Added palindrome partition

---
+++ Python/palindrome_partition.py
@@ -0,0 +1,44 @@
+#palindrome_partition
+
+#Method to check whether the string is palindrome or not
+def isPalindrome(string:str, start:int, end:int): 
+    while start < end: 
+        if string[end] != string[start]: 
+            return False
+        start += 1
+        end -= 1
+    return True
+
+#Recursive method to search for all the possible partitions.
+def findAllPartitions(allPartitions, subPartitions, initial, n, string):
+    if initial >= n:
+        x = subPartitions.copy() 
+        allPartitions.append(x)
+        return
+
+    for i in range(initial, n): 
+        if isPalindrome(string, initial, i): 
+ 
+            subPartitions.append(string[initial:i + 1]) 
+
+            findAllPartitions(allPartitions, subPartitions, i + 1, n, string) 
+
+            subPartitions.pop() 
+
+#Method to print all the possible palindrome partitions.
+def palindromePartitions(string):
+    allPartitions = []
+    subPartitions = []
+
+    findAllPartitions(allPartitions, subPartitions, 0, len(string), string)
+    
+    for i in range(len(allPartitions)): 
+        for j in range(len(allPartitions[i])): 
+            print(allPartitions[i][j], end = " ") 
+        print() 
+
+#Take a string input
+string = str(input())
+palindromePartitions(string) 
+
+
