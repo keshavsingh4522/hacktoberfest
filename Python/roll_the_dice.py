a8a74abccd4f90c5ed93c6b5a3b290d9dbbd16aa
IbadMukrom <ibadmukrom569@gmail.com>
2020-10-19 23:44:48 +0800

    role dice

--- Python/roll_the_dice.py
+++ Python/roll_the_dice.py
@@ -1,13 +1,51 @@
-# simple program to perform roll dice
-import random
-
-min = 1
-max = 6
-roll = 'y'
-
-while roll == 'y':
-    print('rolling the dice. . . ')
-    print('the dice is : ') 
-    print(random.randint(min, max))
-
-    roll = input('roll again? y/n : ')
+import random 
+  
+  
+x = "y"
+   
+while x == "y": 
+      
+    # Gnenerates a random number 
+    # between 1 and 6 (including 
+    # both 1 and 6) 
+    no = random.randint(1,6) 
+      
+    if no == 1: 
+        print("[-----]") 
+        print("[     ]") 
+        print("[  0  ]") 
+        print("[     ]") 
+        print("[-----]") 
+    if no == 2: 
+        print("[-----]") 
+        print("[ 0   ]") 
+        print("[     ]") 
+        print("[   0 ]") 
+        print("[-----]") 
+    if no == 3: 
+        print("[-----]") 
+        print("[     ]") 
+        print("[0 0 0]") 
+        print("[     ]") 
+        print("[-----]") 
+    if no == 4: 
+        print("[-----]") 
+        print("[0   0]") 
+        print("[     ]") 
+        print("[0   0]") 
+        print("[-----]") 
+    if no == 5: 
+        print("[-----]") 
+        print("[0   0]") 
+        print("[  0  ]") 
+        print("[0   0]") 
+        print("[-----]") 
+    if no == 6: 
+        print("[-----]") 
+        print("[0 0 0]") 
+        print("[     ]") 
+        print("[0 0 0]") 
+        print("[-----]") 
+          
+    x=input("press y to roll again and n to exit:") 
+    print("\n") 
