a4f62668ae7c5d5d7a658bc2fbe9e054583562a5
AyushTomar1 <73213195+AyushTomar1@users.noreply.github.com>
2020-10-21 11:49:39 +0530

    Update even or odd.py

--- Python/even or odd.py
+++ Python/even or odd.py
@@ -1,7 +1,6 @@
 #simple way of finding odd and even.
 num = int(input("Enter any integer number: ")) #num variable to store any integer number
-mod = num % 2                                  #mod variable to store value
-if mod > 0:                                    #condition for odd and even
-    print("This is an odd number.")
+if num&1:
+  print("Num is Odd")
 else:
-    print("This is an even number.")
+  print("Num is Even")
