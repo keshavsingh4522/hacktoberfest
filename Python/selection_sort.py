8255631516e58fa58701ac2abe2c614c5faf1b12
Namita Mutha <35648644+namitamutha@users.noreply.github.com>
2020-10-23 09:46:25 +0530

    Update selection_sort.py

--- Python/selection_sort.py
+++ Python/selection_sort.py
@@ -5,13 +5,13 @@ A = [64, 25, 12, 22, 11]
 for i in range(len(A)): 
       
     #find minimum element in rest of unsorted array 
-    min = i 
+    mini = i 
     for j in range(i+1, len(A)): 
-        if A[min] > A[j]: 
-            min = j 
+        if A[mini] > A[j]: 
+            mini = j 
               
     #swap min element with 1st element        
-    A[i], A[min] = A[min], A[i] 
+    A[i], A[mini] = A[mini], A[i] 
   
 print ("Sorted array") 
 for i in range(len(A)): 
