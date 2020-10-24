6a8196698448e8d23e4f13cb07258e8d7a5e7aa0
tej-oops-dev <73373754+tej-oops-dev@users.noreply.github.com>
2020-10-24 08:37:35 +0530

    Create arrowPattern.c
    C program to print arrow pattern.

---
+++ C/arrowPattern.c
@@ -0,0 +1,16 @@
+//C program to print arrow pattern.
+
+#include<stdio.h>
+
+int main(){
+
+  printf("Arrow Pattern using escape sequences:\n");
+  printf("     /\\\n");
+  printf("    //\\\\\n");
+  printf("   ///\\\\\\\n");
+  printf("  ////\\\\\\\\\n");
+  printf("    ||||\n");
+  printf("    ||||\n");
+
+  return 0;
+}
