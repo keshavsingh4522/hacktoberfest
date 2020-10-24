dc915f861af774e55d3176029834ca1656f1e0cf
tej-oops-dev <73373754+tej-oops-dev@users.noreply.github.com>
2020-10-24 08:23:53 +0530

    Create helathCheckOfPC.py
    Here we see how python can interact with the operating system.

---
+++ Python/helathCheckOfPC.py
@@ -0,0 +1,18 @@
+#Python program to interact with the operating system
+
+import shutil
+import psutil
+
+def check_disk_usage(disk):
+    du = shutil.disk_usage(disk)
+    free = du.free / du.total * 100
+    return free > 20
+
+def check_cpu_usage():
+    usage = psutil.cpu_percent(1)
+    return usage < 75
+
+if not check_disk_usage("/") or not check_cpu_usage():
+    print("ERROR! ")
+else:
+    print("Everything is OK! ")
