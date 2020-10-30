c02f0001d1ac40324f7ec2e2e9bce366f25c95a9
coolpersongit <73704449+coolpersongit@users.noreply.github.com>
2020-10-30 19:48:15 +0530

    Update and rename factorial.py to factors.py

---
+++ Python/factors.py
@@ -0,0 +1,12 @@
+# Python Program to find the factors of a number
+
+# This function computes the factor of the argument passed
+def print_factors(x):
+   print("The factors of",x,"are:")
+   for i in range(1, x + 1):
+       if x % i == 0:
+           print(i)
+
+num = 320
+
+print_factors(num)
