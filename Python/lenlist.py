f2070ee04ff54c869e63610ed63e9129bdafcff0
manspidy <73736036+manspidy@users.noreply.github.com>
2020-10-31 13:38:07 +0530

    Create lenlist.py

---
+++ Python/lenlist.py
@@ -0,0 +1,21 @@
+# Python code to demonstrate 
+# length of list 
+# using naive method 
+
+# Initializing list 
+test_list = [ 1, 4, 5, 7, 8 ] 
+
+# Printing test_list 
+print ("The list is : " + str(test_list)) 
+
+# Finding length of list 
+# using loop 
+# Initializing counter 
+counter = 0
+for i in test_list: 
+	
+	# incrementing counter 
+	counter = counter + 1
+
+# Printing length of list 
+print ("Length of list using naive method is : " + str(counter)) 
