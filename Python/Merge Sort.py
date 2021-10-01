28e5af2aaf99f80dec128b2a72637920c5f66247
AksharSans <sanyalamitrakshar@gmail.com>
2021-10-01 20:15:01 +0530

    Update Merge Sort.py

--- Python/Merge Sort.py
+++ Python/Merge Sort.py
@@ -1,17 +1,11 @@
-
 def mergeSort(arr):
     if len(arr) > 1:
-
         r = len(arr)//2
         leftArr = arr[:r]
         rightArr = arr[r:]
-
         mergeSort(leftArr)
         mergeSort(rightArr)
-
         i = j = k = 0
-
-       
         while i < len(leftArr) and j < len(rightArr):
             if leftArr[i] < rightArr[j]:
                 arr[k] = leftArr[i]
@@ -20,8 +14,6 @@ def mergeSort(arr):
                 arr[k] = rightArr[j]
                 j += 1
             k += 1
-
-       
         while i < len(leftArr):
             arr[k] = leftArr[i]
             i += 1
@@ -37,16 +29,8 @@ def display(arr):
     print()
 if __name__ == '__main__':
     arr = [6, 5, 12, 10, 9, 1]
-    
     print("Original array")
     display(arr)
-    
     mergeSort(arr)
-
     print("Sorted array")
     display(arr)
-
-
-
-
-
