62be114a2fa78e41631ce5fb1e8ff51a5f51db0c
Yashi Shukla <59660649+yashi4001@users.noreply.github.com>
2021-10-02 00:19:34 +0530

    Update KadaneAlgo.java
    Given an array Arr[] of N integers. Find the contiguous sub-array(containing at least one number) which has the maximum sum and return its sum.
    Time Complexity: O(N)
    Auxiliary Space: O(1)

--- Java/KadaneAlgo.java
+++ Java/KadaneAlgo.java
@@ -3,7 +3,7 @@ class KadaneAlgo{
     // n: size of array
     //Function to find the sum of contiguous subarray with maximum sum.
     long maxSubarraySum(int arr[], int n){
-        long currSum=0;
+        long currSum=0; //track current sum
         long sumSoFar=Long.MIN_VALUE;
         
         for(int i=0;i<n;i++){
@@ -16,4 +16,6 @@ class KadaneAlgo{
         return sumSoFar;
         
     }
+//     Time Complexity: O(N)
+//     Auxiliary Space: O(1)
 }
