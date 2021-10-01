f34ceef9c7039f58f8eefeabfdc01fd4401d95e0
Yashi Shukla <59660649+yashi4001@users.noreply.github.com>
2021-10-02 00:16:05 +0530

    Added Kadane Algorithm

---
+++ Java/KadaneAlgo.java
@@ -0,0 +1,19 @@
+class KadaneAlgo{
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    long maxSubarraySum(int arr[], int n){
+        long currSum=0;
+        long sumSoFar=Long.MIN_VALUE;
+        
+        for(int i=0;i<n;i++){
+            currSum+=arr[i];
+            
+            if(currSum<arr[i]) currSum=arr[i]; //check current sum
+            
+            if(sumSoFar<currSum) sumSoFar=currSum; //update max sum
+        }
+        return sumSoFar;
+        
+    }
+}
