bd2747051d66bf432eaa9a441807e066bca7692d
Snehil Saxena <91454963+snehilsaxena6248@users.noreply.github.com>
2021-10-02 20:38:29 +0530

    Update BucketSort.cs

--- C#/BucketSort.cs
+++ C#/BucketSort.cs
@@ -1,72 +1,57 @@
+// C# program to sort an array
+// using bucket sort
 using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+class GFG {
  
-namespace Sorting_Algorithms_In_C_Sharp
-{
-    public static class BucketSort
-    {
-        //Bucket sort breaks a list down into sub-lists, you can then use another algo to sort the sub-lists
-        //Bucketsort isn't a good choice if you don't know the range or distribution of the data
-        //Bucket Sort time complexity
-        //Average case: O(n+k) - k being the number of buckets that were created
-        //Worst case: O(N^2)
+  // Function to sort arr[] of size n
+  // using bucket sort
+  static void bucketSort(float []arr, int n)
+  {
+    if (n <= 0)
+      return;
  
-        //In this case, we will focus on building an algorithm that uses bucketsort to sort an array of integers between 1 and 99
-        //we will also assume that the integers in the passed array are evenly distributed
-        public static List<int> BucketSort1(params int[] x)
-        {
-            List<int> result = new List<int>();
+    // 1) Create n empty buckets
+    List<float>[] buckets = new List<float>[n];
  
-            //Determine how many buckets you want to create, in this case, the 10 buckets will each contain a range of 10
-            //1-10, 11-20, 21-30, etc. since the passed array is between 1 and 99
-            int numOfBuckets = 10;
- 
-            //Create buckets
-            List<int>[] buckets = new List<int>[numOfBuckets];
-            for (int i = 0; i < numOfBuckets; i++)
-                buckets[i] = new List<int>();
- 
-            //Iterate through the passed array and add each integer to the appropriate bucket
-            for (int i = 0; i < x.Length; i++)
-            {
-                int buckitChoice = (x[i] / numOfBuckets);
-                buckets[buckitChoice].Add(x[i]);
-            }
- 
-            //Sort each bucket and add it to the result List
-            //Each sublist is sorted using Bubblesort, but you could substitute any sorting algo you would like
-            for (int i = 0; i < numOfBuckets; i++)
-            {
-                int [] temp = BubbleSortList(buckets[i]);
-                result.AddRange(temp);
-            }
-            return result;
-        }
- 
-        //Bubblesort w/ ListInput
-        public static int[] BubbleSortList(List<int> input)
-        {
-            for (int i = 0; i < input.Count; i++)
-            {
-                for (int j = 0; j < input.Count; j++)
-                {
-                    if (input[i] < input[j])
-                    {
-                        int temp = input[i];
-                        input[i] = input[j];
-                        input[j] = temp;
-                    }
-                }
-            }
-            return input.ToArray();
-        }
-        ////Call in Program.cs to test
-        //int[] x = new int[] { 99, 95, 90, 85, 80, 75, 70, 65, 60, 55, 50, 45, 40, 35, 30, 25, 20, 15, 10, 5, 1 };
-        //List<int> sorted = Bucket_Sort.BucketSort(x);
-        //foreach (int i in sorted)
-        //    Console.WriteLine(i);
+    for (int i = 0; i < n; i++) {
+      buckets[i] = new List<float>();
     }
+ 
+    // 2) Put array elements in different buckets
+    for (int i = 0; i < n; i++) {
+      float idx = arr[i] * n;
+      buckets[(int)idx].Add(arr[i]);
+    }
+ 
+    // 3) Sort individual buckets
+    for (int i = 0; i < n; i++) {
+      buckets[i].Sort();
+    }
+ 
+    // 4) Concatenate all buckets into arr[]
+    int index = 0;
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < buckets[i].Count; j++) {
+        arr[index++] = buckets[i][j];
+      }
+    }
+  }
+ 
+  // Driver code
+  public static void Main()
+  {
+    float []arr = { (float)0.897, (float)0.565,
+                   (float)0.656, (float)0.1234,
+                   (float)0.665, (float)0.3434 };
+ 
+    int n = arr.Length;
+    bucketSort(arr, n);
+ 
+    Console.WriteLine("Sorted array is ");
+    foreach(float el in arr) {
+      Console.Write(el + " ");
+    }
+  }
 }
