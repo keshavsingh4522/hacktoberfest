d0a0da727401bebde0d005f7f44fcb355798effc
Pratik <73239498+pratik-2707@users.noreply.github.com>
2021-10-02 21:21:03 +0530

    Update temperatureConverter.c
    temperature in kelvin available

--- C/temperatureConverter.c
+++ C/temperatureConverter.c
@@ -9,6 +9,6 @@ celsius = 24;
 fahrenheit =( (celsius*9)/5)+32;
 kelvin = celsius + 273.15 ;
 printf("Temperature in fahrenheit is:  %f\n",fahrenheit);  
-printf("Temperature in kelvin     is:  %f",kelvin); 
+printf("Temperature in kelvin      is:  %f",kelvin); 
 return (0); 
 } 
