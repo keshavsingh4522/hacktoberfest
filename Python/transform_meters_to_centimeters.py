c94e0e265c40af007839f5e7b1d21976de73eec5
Amédée DERA <53271706+a-dera@users.noreply.github.com>
2021-10-02 20:38:35 +0000

    Adding more features

--- Python/transform_meters_to_centimeters.py
+++ Python/transform_meters_to_centimeters.py
@@ -1,4 +1,26 @@
 
-meters = float (input ('enter the number in meters:'))
-cent = meters*100
-print ('the num in cent equals {}'.format(cent))
+# conversion in Python
+
+# meter to centimeters
+def meters_to_centimeters():
+  meters = float (input ('enter the number in meters:'))
+  cent = meters*100
+  print ('the num in cent equals {}'.format(cent))
+  
+  # centimeters to millimeters
+  def centimeters_to_millimeters():
+    centimeters = float (input ('enter the number in centimeters:'))
+    milli = centimeters*100
+    print ('the num in millimeter equals {}'.format(milli))
+  
+  # centimeters to metters
+  def centimeters_to_meters():
+    centimeters = float (input ('enter the number in centimeters:'))
+    metter = centimeters/100
+    print ('the num in cent equals {}'.format(metter))
+  
+  # millimeters to meters
+  def millimeters_to_meters():
+    millimeters = float (input ('enter the number in millimeters:'))
+    metter = millimeters/1000
+    print ('the num in cent equals {}'.format(metter))
