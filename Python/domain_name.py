2392b1e72b5dacc04772e7e11e480c1bacef09ca
Amédée DERA <53271706+a-dera@users.noreply.github.com>
2021-10-02 20:30:47 +0000

    Adding the possibility for user to input an url

--- Python/domain_name.py
+++ Python/domain_name.py
@@ -7,15 +7,20 @@ def getHostName(url):
     regex = "\w+\.\w+.\w\w\w"
     host = re.findall(regex,url)
     return(host[0])
-    
-def main():
+   
+# launch this if you want a fast test
+def test():
     url_testA = "http://www.cnn.com"
     url_testB = "https://hacktoberfest.digitalocean.com/profile"
     
     print(getHostName( url_testA))
     print(getHostName( url_testB))
-   
-   
+  
+# launch this if you have a url to check
+def main():
+    url = input ('Enter a website url')
+    print(getHostName( url))
+	
 main()
    
    
