121d53b56d29e8ab784ace1e0128835b33da9223
peprick <56351693+peprick@users.noreply.github.com>
2021-10-02 21:38:13 +0530

    The correct solution to the problem.
    This is the correct solution because previous code replaced vowels with unnecessary gaps.
    This code is also time optimized but takes up O(n) space.

--- CPP/Delete_vowels.cpp
+++ CPP/Delete_vowels.cpp
@@ -1,22 +1,22 @@
 //Program to delete all the vowels from a given string and print the resulting string 
 
 #include<iostream>
+#include<string>
 using namespace std;
 
 int main()
 {
-        char str[100];
+        string st,ans="";
+        
         cout<<"\n Enter String : ";
-        cin>>str;
+        cin>>st;
 
-        for(int i=0; str[i]!='\0'; i++)
+        for(int i=0; i<st.size() ; i++)
         {
-                if (str[i]=='a' || str[i]=='e' || str[i]=='i' || str[i]=='o' || str[i]=='u' || str[i]=='A' || str[i]=='E' || str[i]=='I'
-|| str[i]=='O' || str[i]=='U')
-                {
-                        str[i]=' ';
-                }
+                if (tolower(st[i])!='a' && tolower(st[i])!='e' && tolower(st[i])!='i' && tolower(st[i])!='o' && tolower(st[i])!='u')
+                
+                ans+=st[i];
         }
-        cout<<"\n String After Deleting Vowels : "<<str;
+        cout<<"\n String After Deleting Vowels : "<<ans;
         return 0;
 }
