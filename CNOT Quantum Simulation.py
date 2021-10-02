3ee6c2d79282f30f1fc0e5ccfe136e4291c650c8
AksharSans <sanyalamitrakshar@gmail.com>
2021-10-02 15:25:53 +0530

    Added CNOT Quantum Simulation.py

---
+++ CNOT Quantum Simulation.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+import qiskit as qk
+
+
+# In[10]:
+
+
+q = qk.QuantumRegister(2) # Initiating 2 qubits
+c = qk.ClassicalRegister(2) # Initiating 2 classical bits
+circuit = qk.QuantumCircuit(q,c) # Preparing the Quantum Circuit
+circuit.h(q[0]) # Adding a Hadamard Gate to Qubit 0 to superpose with Qubit 1
+circuit.cx(q[0] , q[1]) # CNOT gate added as esplained: Qubit 0 and Qubit 1 connected
+circuit.measure(q , c) # Measuring
+print(circuit)
+
+
+# In[12]:
+
+
+simulator = qk.BasicAer.get_backend('qasm_simulator') # Using a Quantum Simulator on Local Machine for simulation
+job = qk.execute(circuit , simulator)
+output = job.result()
+
+counts = output.get_counts(circuit) # get_counts returns the binary outcome
+print(counts)
+
