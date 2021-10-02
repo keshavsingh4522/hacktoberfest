f76791342454eb9c54c8df1416c21c41c893815d
Ujjwal Singh Basnet <ujjwalbas167@gmail.com>
2021-10-02 19:54:38 +0545

    sorting using js

---
+++ JS/sorting/quick_sort.js
@@ -0,0 +1,43 @@
+/******************************* Quick SORT ******************************/
+
+const swap = (a,b) => [a,b] = [b,a]
+
+var items = [5,3,7,6,2,9];
+function partition(items, left, right) {
+    var pivot   = items[Math.floor((right + left) / 2)], //middle element
+        i       = left, //left pointer
+        j       = right; //right pointer
+    while (i <= j) {
+        while (items[i] < pivot) {
+            i++;
+        }
+        while (items[j] > pivot) {
+            j--;
+        }
+        if (i <= j) {
+            swap(items[i],items[j]); //sawpping two elements
+            i++;
+            j--;
+        }
+    }
+    return i;
+}
+
+function quickSort(items, left, right) {
+    var index;
+    if (items.length > 1) {
+        index = partition(items, left, right); //index returned from partition
+        if (left < index - 1) { //more elements on the left side of the pivot
+            quickSort(items, left, index - 1);
+        }
+        if (index < right) { //more elements on the right side of the pivot
+            quickSort(items, index, right);
+        }
+    }
+    return items;
+}
+// first call to quick sort
+var sortedArray = quickSort(items, 0, items.length - 1);
+console.log(sortedArray); //prints [2,3,5,6,7,9]
+
+/******************************* Quick SORT ******************************/
