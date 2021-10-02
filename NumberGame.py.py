9fa5f428cdea99eb4f1e6f9b375703b626f16597
Amédée DERA <53271706+a-dera@users.noreply.github.com>
2021-10-02 20:54:15 +0000

    Update NumberGame.py.py

--- NumberGame.py.py
+++ NumberGame.py.py
@@ -1,19 +1,23 @@
 import random
 
-while True:
-    a=random.randint(1,100)
-    b=None
-    while a!=b:
-        b=int(input("Guess a Number between (1-100): "))
-        if a<b:
-            print("Your guess is too high")
-        elif a>b:
-            print("Your guess is too low")
-    print("Congratulations! You gussed the number correctly.")
-    print()
-    z=input("You want to play again (y/n): ")
-    print()
-    if z=="n" or z=="N":
-        break
+#use best practices.. Always put sections of code in functions.
+def guess_number():
+    while True:
+        a=random.randint(1,100)
+        b=None
+        while a!=b:
+            b=int(input("Guess a Number between (1-100): "))
+            if a<b:
+                print("Your guess is too high")
+            elif a>b:
+                print("Your guess is too low")
+        print("Congratulations! You gussed the number correctly.")
+        print()
+        z=input("You want to play again (y/n): ")
+        print()
+        if z=="n" or z=="N":
+            break
 
+            
+ guess_number()
         
