c0bf5a8b023bd5e6ac5ef3153dfe18f6ca3a67bc
Abhishek Nallana <60889353+abhinallana@users.noreply.github.com>
2021-10-03 19:44:39 +0530

    Updated Comments to functions and Params

--- Java/AgeCounter.java
+++ Java/AgeCounter.java
@@ -21,12 +21,14 @@ public class AgeCounter {
         int day=bday.nextInt();
         
         
-        
+        //analyses todays Date and your birthDate
         LocalDate pres = LocalDate.now();
         LocalDate bdate = LocalDate.of(year,month,day);
         
+        //function to get the present age
         int age = Period.between(bdate , pres).getYears();
         
+        //Prints present, birthDate, Present Age
         System.out.println(pres);
         System.out.println(bdate);
         System.out.println(age);
