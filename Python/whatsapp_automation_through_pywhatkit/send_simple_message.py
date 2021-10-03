1ae067bc96772ab487304adb1f69b683fdafe583
Parmanand Patidar <56157826+parmanand99@users.noreply.github.com>
2021-10-03 15:17:28 +0530

    a script to send whatsapp message with the use of pywhatkit module.
    send_simple_message.py contains code for sending message instantly and at schedule time, I will upload another file for further use cases.

---
+++ Python/whatsapp_automation_through_pywhatkit/send_simple_message.py
@@ -0,0 +1,3 @@
+import pywhatkit as kit
+kit.sendwhatmsg_instantly("+91xxxxxxxxxxx", "sending instant message ") # send a message instantly 
+kit.sendwhatmsg("+91xxxxxxxxxx"," sending scheduled message " , 14,59)  #send a message at specified hour and minute in 24 hour format
