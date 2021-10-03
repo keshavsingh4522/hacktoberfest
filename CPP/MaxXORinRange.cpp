d3a952bfe78cd7ea91af955bc4f016d4ae84f4e8
Mandeep Taneja <mandeeptaneja21@outlook.com>
2021-10-03 15:30:46 +0530

    Create MaxXORinRange.cpp

---
+++ CPP/MaxXORinRange.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+
+#define ll long long int
+#define pi 3.1415926535897932385
+#define mod 1000000007
+#define db double
+#define pp(x) pair<x,x>
+#define ff first
+#define ss second
+#define FIO ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define tc ll tc; cin>>tc ; cin.ignore(5,'\n'); for(ll number = 1 ; number <= tc ; number ++ )
+#define vv(x) vector<x >
+#define pb push_back
+#define pob pop_back
+#define pf push_front
+#define pof  pop_front
+#define endl "\n"
+#define umap unordered_map
+#define pq priority_queue
+#define dbg(x) { cout<< #x << ": " << (x) << endl; }
+#define dbg2(x,y) { cout<< #x << ": " << (x) << " , " << #y << ": " << (y) << endl; }
+#define bp ll kkk; cin >>kkk;
+#define dbgArr(a,n) cout<<"Array "<<#a<<endl; for(ll i=0 ; i<n ; i++) cout<<a[i]<<" "; cout<<endl;
+#define forn(i,n) for(ll i=0 ; i<n ; i++)
+#define forn1(i,n) for(ll i=1 ; i<=n ; i++)
+#define arr(a,n) for(ll i=0;i<n;i++){ cin>>a[i]; }
+#define arr1(a,n) for(ll i=1;i<=n;i++){ cin>>a[i]; }
+#define file freopen("inp.in","r",stdin); freopen("outp.out","w",stdout)
+#define print cout<<"Case #"<<number<<": "
+#define all(x) x.begin(), x.end()
+#define trav(x, a) for(auto &x: a)
+ll power(ll x, ll b) {ll p = 1;while (b > 0) {if(b&1) {p = p * x; p %= mod;} b >>= 1; x *= x; x %= mod;} return p % mod;}
+
+using namespace std;
+
+int main()
+{
+    FIO
+    ll a,b ;
+    cin>>a>>b ;
+    bitset<64> ba , bb ;
+    ba = bitset<64>(a);
+    bb = bitset<64>(b);
+    bitset<64> ans ;
+    bool ucbf=false ;
+    for(ll i=63 ; i>=0 ; i--)
+    {
+        if(bb[i]==ba[i])
+        {
+            if(ucbf) ans[i]=1 ;
+            else ans[i]=0 ;
+        }
+        else if(bb[i]!=ba[i])
+        {
+            ans[i]=1 ;
+            ucbf=true ;
+        }
+    }
+	cout<<ans.to_ulong();
+    return 0;
+}
