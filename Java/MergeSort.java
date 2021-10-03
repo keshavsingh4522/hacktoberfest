25324ae05b595ceef7259d86cfa9329fa860fc72
ashwithpoojary98 <61794427+ashwithpoojary98@users.noreply.github.com>
2021-10-03 09:48:32 +0530

    Merge sort using java
    Merge sort is one of the most efficient sorting algorithms. It works on the principle of Divide and Conquer.

--- Java/MergeSort.java
+++ Java/MergeSort.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 import java.util.Scanner;
-
+//Merge sort
 public class MergeSort {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
