558e574cf2f67575b19b7b842bde948933678a08
prashant kumar <47036071+pkkushagra@users.noreply.github.com>
2021-10-04 08:39:48 +0530

    Create matrixprint.py

---
+++ Python/matrixprint.py
@@ -0,0 +1,20 @@
+  
+R = int(input("Enter the number of rows:"))
+C = int(input("Enter the number of columns:"))
+  
+# Initialize matrix
+matrix = []
+print("Enter the entries rowwise:")
+  
+# For user input
+for i in range(R):          # A for loop for row entries
+    a =[]
+    for j in range(C):      # A for loop for column entries
+         a.append(int(input()))
+    matrix.append(a)
+  
+# For printing the matrix
+for i in range(R):
+    for j in range(C):
+        print(matrix[i][j], end = " ")
+    print()
