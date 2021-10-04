95a012a13f08e00d51ee7da6eb2fe6b1ef655b50
Priyanshi Goel <82112540+priyanshi-git@users.noreply.github.com>
2021-10-05 00:13:57 +0530

    Update even or odd.py
    I believe using the classical mathematic logic is far more understandable.

--- Python/even or odd.py
+++ Python/even or odd.py
@@ -1,6 +1,6 @@
 #simple way of finding odd and even.
 num = int(input("Enter any integer number: ")) #num variable to store any integer number
-if num&1:
-  print("Num is Odd")
-else:
+if num%2==0:
   print("Num is Even")
+else:
+  print("Num is Odd")
