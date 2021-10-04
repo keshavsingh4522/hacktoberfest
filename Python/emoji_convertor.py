20eb6ab9cc184a29521072c80a332306030a4c6b
MOHAMMAD SHAIBAZ QAISAR <76586987+MOHAMMADSHAIBAZQAISAR@users.noreply.github.com>
2021-10-04 16:47:47 +0530

    Create emoji_convertor.py
    Added an algorithm..
    Here is an simple code!! which converts your mood into an emoji...
    for example if you are happy and write :) this symbol in the terminal then it gonna give you a emoji of smile..Is'nt it great guys..
    if you are sad then ,type this :( in the terminal you gonna get a sad emoji.. There's a little code up there...
    If you are keen to make more emoji's then jst add few more attributes in the emojis list..
    If you are using windows then "hold the Windows key down and press either the period (.) or semicolon (;) key " for having emoji's in the editor
    if it's an mac os then Press "Command + Control + Space".

---
+++ Python/emoji_convertor.py
@@ -0,0 +1,16 @@
+
+def emojiconverotr(words) :
+    message = words.split(' ')
+    emojis = {
+        ":)": "😊",
+        ":(": "☹"
+
+    }
+    output = ""
+    for word in message:
+        output += emojis.get(word, word) + " "
+    print(output)
+
+
+words = input("> ")
+emojiconverotr(words)
