bd1ee369ce962ad9bd0c84309d55eabd1a0fb0e1
harshitgupta2000 <56402525+harshitgupta2000@users.noreply.github.com>
2021-10-04 13:11:40 +0530

    Caesar Cipher

---
+++ Python/Caesar cipher.py
@@ -0,0 +1,13 @@
+def encrypt(text,s):
+result = ""
+   # transverse the plain text
+   for i in range(len(text)):
+      char = text[i]
+      # Encrypt uppercase characters in plain text
+      
+      if (char.isupper()):
+         result += chr((ord(char) + s-65) % 26 + 65)
+      # Encrypt lowercase characters in plain text
+      else:
+         result += chr((ord(char) + s - 97) % 26 + 97)
+      return result
