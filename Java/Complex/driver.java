2d1eeb9180fde0d5a55442e113aae79cfedcf556
iiriis <55133414+iiriis@users.noreply.github.com>
2021-10-06 04:32:29 +0530

    Update driver.java

--- Java/Complex/driver.java
+++ Java/Complex/driver.java
@@ -5,8 +5,8 @@ class Main
 	public static void main(String args[])
 	{
 	
-		complex c1 = new Complex(7,5); // 7 + 5i
-		complex c2 = new Complex(8,4); // 8 + 4i
+		complex c1 = new complex(7,5); // 7 + 5i
+		complex c2 = new complex(8,4); // 8 + 4i
 		
 		complex rez = c1.add(c2);
     System.out.println(rez);
