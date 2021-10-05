9f710a1a7e8ecc04e90c10c19916869a06728c27
iiriis <55133414+iiriis@users.noreply.github.com>
2021-10-06 04:25:23 +0530

    Added driver class

---
+++ Java/Complex/driver.java
@@ -0,0 +1,17 @@
+
+class Main
+{
+	
+	public static void main(String args[])
+	{
+	
+		complex c1 = new Complex(7,5); // 7 + 5i
+		complex c2 = new Complex(8,4); // 8 + 4i
+		
+		complex rez = c1.add(c2);
+    System.out.println(rez);
+		
+	}
+	
+}
+	
