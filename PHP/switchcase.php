51ead8a15ad2abaa58f1ded75e300d3e538da16d
Muhammad Zul Marij Rizqi Fathullah <muhammadzulmarijrizqifathullah@gmail.com>
2021-10-05 21:11:35 +0700

    Create switchcase.php

---
+++ PHP/switchcase.php
@@ -0,0 +1,14 @@
+<?php
+$color = "blue";
+
+switch ($color) {
+  case "red":
+    echo "color is red!";
+    break;
+  case "blue":
+    echo "color is blue!";
+    break;
+  default:
+    echo "color is not blue and red!";
+}
+?>
