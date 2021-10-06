ac1aa84045ed6e464e5844dcb9d37050de9591b1
Shubham <89636139+shubhamcloud9@users.noreply.github.com>
2021-10-06 11:46:22 +0530

    Update Fibonacci.js

--- JS/Fibonacci.js
+++ JS/Fibonacci.js
@@ -1,10 +1,13 @@
-//Code for Fibonacci series in JavaScript
-const nthFibonacci = (n) => {
-  // set the base case(fib[0] = 0, fib[1] = 1)
-  let fib = [0, 1]
-  for (let i = 2; i <= n; i++) {
-      fib.push(fib[i - 1] + fib[i - 2]);
-  }
+//start of the program
 
-  return fib[n];
+var i;
+var fib = []; // Initialize array!
+
+fib[0] = 0;
+fib[1] = 1;
+for (i = 2; i <= 10; i++) {
+  // Next fibonacci number = previous + one before previous
+  // Translated to JavaScript:
+  fib[i] = fib[i - 2] + fib[i - 1];
+  console.log(fib[i]);
 }
