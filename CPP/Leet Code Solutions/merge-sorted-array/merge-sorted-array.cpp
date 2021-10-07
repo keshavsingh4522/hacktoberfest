1d8cb9dd64163dba7888125d89b5ef21f7a0dcf6
Shouhardik Saha <60508616+shouhardik@users.noreply.github.com>
2021-10-07 14:29:21 +0530

    Update merge sorted arrays

--- CPP/Leet Code Solutions/merge-sorted-array/merge-sorted-array.cpp
+++ CPP/Leet Code Solutions/merge-sorted-array/merge-sorted-array.cpp
@@ -1,24 +1,30 @@
-// Method - 1
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        for(int i=0;i<n;i++){
-            nums1[i+m] = nums2.at(i);
+        
+        int i=0,j=0,k=0;
+        vector<int> arr(m+n);
+        while(i<m && j<n){
+            if(nums1[i]<nums2[j]){
+                arr[k++]=nums1[i++];
+                
+            }else{
+                arr[k++]=nums2[j++];
+               
+            }
+            
         }
-        sort(nums1.begin(),nums1.end());
-        return;
-    }
-};
-
-// Method - 2
-class Solution {
-public:
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        nums1.resize(m);
-        for(int i=0;i<n;i++){
-            nums1.push_back(nums2.at(i));
+        while(i<m){
+            arr[k++]=nums1[i++];
         }
-        sort(nums1.begin(),nums1.end());
-        return;
+        while(j<n){
+            arr[k++]=nums2[j++];
+        }
+        int s=sizeof(arr)/sizeof(arr[0]);
+        for(i=0;i<m+n;i++){
+            nums1[i]=arr[i];
+            //cout<<arr[i]<<' ';
+        }
+        
     }
 };
