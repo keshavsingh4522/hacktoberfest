876cebd3f21919350b3d8a29dc83f8aab839e85f
Ishita Chattopadhyay <60072783+ishita2104@users.noreply.github.com>
2021-10-07 19:53:02 +0530

    Update single-number.cpp

--- CPP/Leet Code Solutions/single-number/single-number.cpp
+++ CPP/Leet Code Solutions/single-number/single-number.cpp
@@ -1,20 +1,13 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        if(nums.size()==1){
-            return nums[0];
+        int res;
+        res=nums[0];
+        for(int i=1;i<nums.size();i++)
+        {
+            res=res^nums[i];
         }
-        set<int> nums_set(nums.begin(), nums.end());
-        int set_sum = 0;
-        int total_sum = 0;
-        for(int x:nums_set){
-            set_sum += x;
-        }
-        for(int x:nums){
-            total_sum += x;
-        }
-        int single_num = 2*set_sum - total_sum;
-        return single_num;
+        return res;
         
     }
 };
