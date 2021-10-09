ad916c8bdf9df8c61b4e4d71cc95a9918ac49bdf
Sudhakar B <56016120+SudhaKarRanji@users.noreply.github.com>
2021-10-09 16:04:57 +0530

    Create PyQT.py

---
+++ Python/PyQT.py
@@ -0,0 +1,7 @@
+from PyQt5.QtWidgets import QApplication, QLabel
+app = QApplication([])
+label = QLabel("HEllo world")
+label = QLabel("------**Hacktober Fest 2021**-------")
+label.show()
+
+app.exec_()
