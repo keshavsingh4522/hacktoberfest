08a8e5bdecd0c5038c316f8d971d493a52a5ab1f
heetboda10 <heetboda10@gmail.com>
2021-10-09 20:45:36 +0530

    added JS file to convert JSON to string

---
+++ JS/jsonToString.js
@@ -0,0 +1,6 @@
+function jsonToString(json) {
+    return JSON.stringify(json)
+}
+  
+console.log(jsonToString({name: "John", age: 30, city: "New York"}))
+console.log(jsonToString({name: "Harry", age: 38, city: "Texas"}))
