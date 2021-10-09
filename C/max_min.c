39530e9d29ecbb291ecdc2bf140a8fa15fb88ce6
Harsh Sharma <80146213+harsh-sharma-z@users.noreply.github.com>
2021-10-09 21:37:30 +0530

    Update max_min.c
    The code is longer than the former. But it is much faster. Please have a look.

--- C/max_min.c
+++ C/max_min.c
@@ -1,28 +1,64 @@
-#include<stdio.h>
+#include <limits.h>
+#include <stdio.h>
+  
+// Recursive function to find the minimum
+// and the maximum element of the array
+void recursiveMinMax(int arr[], int N,
+                     int* minE, int* maxE)
+{
+    // Base Case
+    if (N < 0) {
+        return;
+    }
+  
+    // If current element is smaller
+    // than minE then update it
+    if (arr[N] < *minE) {
+        *minE = arr[N];
+    }
+  
+    // If current element is greater
+    // than maxE then update it
+    if (arr[N] > *maxE) {
+        *maxE = arr[N];
+    }
+  
+    // Recursive call for next iteration
+    recursiveMinMax(arr, N - 1, minE, maxE);
+}
+  
+// Function to find the minimum and
+// maximum element of the array
+void findMinimumMaximum(int arr[], int N)
+{
+    int i;
+  
+    // variable to store the minimum
+    // and maximum element
+    int minE = INT_MAX, maxE = INT_MIN;
+  
+    // Recursive Function to find
+    // minimum & maximum element
+    recursiveMinMax(arr, N - 1, &minE, &maxE);
+  
+    // Print the minimum and maximum element
+    printf("The minimum element is %d", minE);
+    printf("\n");
+    printf("The maximum element is %d", maxE);
+  
+    return;
+}
+  
+// Driver Code
 int main()
 {
- int n, i, max, min, a[100];
- printf("Enter total number of elements:");
- scanf("%d",&n);
- printf("Enter values:");
- for (i=0;i<n;i++)
- {
- 	scanf("%d", &a[i]);
- }
- max=a[0];
- min=a[0];
- for (i=1;i<n;i++)
- {
- 	if (a[i]>max)
- 	{
- 		max=a[i];
-	}
-	if (a[i]<min)
- 	{
- 		min=a[i];
-	}
- }
- printf("Maximum element: %d\n", max);
- printf("Minimum element: %d", min);
- return 0;
- }
+  
+    int n;
+	printf("Enter size of array");
+	scanf("%d",&n);
+    int arr[n];
+  
+    
+    findMinimumMaximum(arr, n);
+    return 0;
+}
