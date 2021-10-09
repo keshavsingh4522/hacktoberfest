f3a183dc492328a000d3d928244fb6a823c963a4
Harsh Sharma <80146213+harsh-sharma-z@users.noreply.github.com>
2021-10-09 21:43:12 +0530

    Update temperatureConverter.c
    Now the user can choose what he/she wants to perform. And also enter the temperature on their own! please have a look.

--- C/temperatureConverter.c
+++ C/temperatureConverter.c
@@ -1,13 +1,30 @@
-//temperature converter from celsius to fahrenheit
-
-#include<stdio.h>
-
-int main()   
-{   
-float fahrenheit, celsius;  
-celsius = 24;  
-fahrenheit =( (celsius*9)/5)+32;  
-printf("Temperature in fahrenheit is:  %f",fahrenheit);  
-return (0); 
-    
-} 
+#include <stdio.h>
+ 
+int main()
+{
+  
+    float fh,cl;
+    int choice;
+ 
+    printf("\n1: Convert temperature from Fahrenheit to Celsius."); 
+    printf("\n2: Convert temperature from Celsius to Fahrenheit.");
+    printf("\nEnter your choice (1, 2): ");
+    scanf("%d",&choice);
+ 
+    if(choice ==1){
+        printf("\nEnter temperature in Fahrenheit: ");
+        scanf("%f",&fh);
+        cl= (fh - 32) / 1.8;
+        printf("Temperature in Celsius: %.2f",cl);
+    }
+    else if(choice==2){
+        printf("\nEnter temperature in Celsius: ");
+        scanf("%f",&cl);
+        fh= (cl*1.8)+32;
+        printf("Temperature in Fahrenheit: %.2f",fh);
+    }
+    else{
+        printf("\nInvalid Choice !!!");
+    }
+    return 0;
+}
