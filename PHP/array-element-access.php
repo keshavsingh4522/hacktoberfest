293999f1a9016884feb07d46ce3d5a6fb34cf702
Aashutosh Sapkota <55585284+Aashutoshbro@users.noreply.github.com>
2021-10-10 13:02:21 +0545

    Create array-element-access.php

---
+++ PHP/array-element-access.php
@@ -0,0 +1,15 @@
+<?php
+$array = array(
+    "foo" => "bar",
+    42    => 24,
+    "multi" => array(
+         "dimensional" => array(
+             "array" => "foo"
+         )
+    )
+);
+
+var_dump($array["foo"]);
+var_dump($array[42]);
+var_dump($array["multi"]["dimensional"]["array"]);
+?>
