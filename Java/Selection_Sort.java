0f5aa9204fb640051b4416135dbc8aaa7b4d118f
Sagar Raj Gantayat <78720488+sagarcode28@users.noreply.github.com>
2021-10-10 10:24:29 +0530

    Create Selection_Sort.java

---
+++ Java/Selection_Sort.java
@@ -0,0 +1,42 @@
+//Sorting Array Using Selection Sort.
+
+
+public class Selection_Sort{
+    public static void main(String[] args) {
+        int[] arr = {1, 2, -4, -3, 44, 9, -23};
+        Selection(arr); //sorting array elements using Selection sort
+        System.out.println(Arrays.toString(arr));
+    }
+
+    //    Selection Sort
+    static void Selection(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int last = arr.length - i -1;
+            int max = Max(last, arr);
+            swap(max, arr, last);
+        }
+    }
+
+    //    Swapping of Max to Last
+    static void swap(int max, int[] arr, int last) {
+        if(arr[max] != arr[last]){
+            int temp = arr[max];
+            arr[max] = arr[last];
+            arr[last] = temp;
+        }
+    }
+
+
+    //    finding the Maximum Number Index
+    static int Max(int last, int[] arr) {
+        int max = 0;
+        for(int  i = 0; i <= last; i++){
+            if(arr[i] >= arr[max]){
+                max = i;
+            }
+        }
+        return max;
+    }
+
+
+}
