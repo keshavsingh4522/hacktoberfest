0ffe611bb680db7434bd5cb2af814cc52bc3ffe0
coders-group11 <92266181+coders-group11@users.noreply.github.com>
2021-10-10 21:55:20 +0530

    Create knapsack.c

---
+++ knapsack.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int max(int a, int b) {
+   if(a>b){
+      return a;
+   } else {
+      return b;
+   }
+}
+int knapsack(int W, int wt[], int val[], int n) {
+   int i, w;
+   int knap[n+1][W+1];
+   for (i = 0; i <= n; i++) {
+      for (w = 0; w <= W; w++) {
+         if (i==0 || w==0)
+            knap[i][w] = 0;
+         else if (wt[i-1] <= w)
+            knap[i][w] = max(val[i-1] + knap[i-1][w-wt[i-1]], knap[i-1][w]);
+         else
+            knap[i][w] = knap[i-1][w];
+      }
+   }
+   return knap[n][W];
+}
+int main() {
+   int val[] = {20, 25, 40};
+   int wt[] = {25, 20, 30};
+   int W = 50;
+   int n = sizeof(val)/sizeof(val[0]);
+   printf("The solution is : %d", knapsack(W, wt, val, n));
+   return 0;
+}
