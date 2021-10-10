01b93834c83ffa9b9c2868312d0a7fdaf79fa436
Divyesh Vishwakarma <divyesh1099@gmail.com>
2021-10-10 17:46:55 +0530

    Web Development Projects added

---
+++ Web Development Projects/Django/Simple Blog Website/blog/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from .models import Post, Comment
+# Register your models here.
+# admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'publish', 'status')
+    list_filter = ('status', 'created', 'publish', 'author')
+    search_fields = ('title', 'body')
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
+    date_heirarchy = 'publish'
+    ordering = ('status', 'publish')
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'post', 'created', 'active')
+    list_filter = ('active', 'created', 'updated')
+    search_fields = ('name', 'email', 'body')
---
+++ Web Development Projects/Django/Simple Blog Website/mysite/local_settings.py
@@ -0,0 +1,11 @@
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+DEBUG = True
