430fc51ad743d510f1383a6ec05d69ba3bb54576
Ansh Rastogi <60509184+anshrastogi240901@users.noreply.github.com>
2021-10-10 19:22:14 +0530

    Add files via upload

---
+++ PHP/MentalHealing Blogs/index.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" 
+    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+   <link href="https://fonts.googleapis.com/css2?family=Marck+Script&display=swap" rel="stylesheet">
+   <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap" rel="stylesheet">
+   <link rel="icon" href="logo.png">
+    <title>Attaining Peace | Home</title>
+    <style>
+      .carousel-control-prev-icon,
+.carousel-control-next-icon {
+  height: 90px;
+  width: 90px;
+  outline: black;
+  background-size: 100%, 100%;
+  border-radius: 50%;
+  border: 2px solid black;
+  background-image: none; }
+.carousel-control-next-icon:after
+{ content: '>';
+  font-size: 55px;
+  color: red;}
+.carousel-control-prev-icon:after {
+  content: '<';
+  font-size: 55px;
+  color: red;}
+    </style>
+  </head>
+  <body><?php include "header.php"?>
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel" data-interval="1500">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="books.jpg" alt="Motivational Book Reviews">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="blog.jpg" alt="Blogs">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="movies.jpg" alt="Motivational Movie Reviews">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="music.png" alt="Soul Soothing Music">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="podcast.png" alt="Life-changing Podcasts">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true" style="color:black;"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a></div>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
