5dd2274224c7df9612e4e47511a1fa202e11f5bd
Sonal Mahajan <69248747+sonal019@users.noreply.github.com>
2021-10-12 18:00:52 +0530

    Update and rename Plagiarism Checker to Plagiarism Checker.py

--- Plagiarism Checker.py
+++ Plagiarism Checker.py
@@ -1,22 +1,17 @@
-#Python code for plagiarism checker
 pip install scikit-learn
 import os
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-
 student_files = [doc for doc in os.listdir() if doc.endswith('.txt')]
 student_notes = [open(_file, encoding='utf-8').read()
                  for _file in student_files]
 
-
 def vectorize(Text): return TfidfVectorizer().fit_transform(Text).toarray()
 def similarity(doc1, doc2): return cosine_similarity([doc1, doc2])
-
 
 vectors = vectorize(student_notes)
 s_vectors = list(zip(student_files, vectors))
 plagiarism_results = set()
-
 
 def check_plagiarism():
     global s_vectors
@@ -30,7 +25,6 @@ def check_plagiarism():
             score = (student_pair[0], student_pair[1], sim_score)
             plagiarism_results.add(score)
     return plagiarism_results
-
 
 for data in check_plagiarism():
     print(data)
