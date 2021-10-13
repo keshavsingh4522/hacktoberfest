fe173b55a7c0c0e4b91324c7e611fd4e16e53484
Shivani <78358314+Shivani0701@users.noreply.github.com>
2021-10-13 22:37:35 +0530

    Problem Statement

---
+++ CPP/Leet Code Solutions/Three Sum/Problem.cpp
@@ -0,0 +1,24 @@
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+
+ 
+
+Example 1:
+
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+Example 2:
+
+Input: nums = []
+Output: []
+Example 3:
+
+Input: nums = [0]
+Output: []
+ 
+
+Constraints:
+
+0 <= nums.length <= 3000
+-105 <= nums[i] <= 105
