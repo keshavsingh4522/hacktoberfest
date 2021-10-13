87c1c17892aa1d37909b93e62abaa62a3695005e
ilyas829 <72152969+ilyas829@users.noreply.github.com>
2021-10-13 19:51:04 +0530

    Improved Code
    Removed '?' and ' : ' ,  and also Added ' ; ' due to which program was showing error

--- C/Linear_Search_2.c
+++ C/Linear_Search_2.c
@@ -21,7 +21,7 @@ int main(void)
 	// Function call
 	int result = search(arr, n, x);
 	(result == -1)
-		? printf("Element is not present in array")
-		: printf("Element is present at index %d", result);
+		 printf("Element is not present in array");
+		 printf("Element is present at index %d", result);
 	return 0;
 }
