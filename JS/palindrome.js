834590bfe668165c1bb9b4b75020d0cf6f09ab29
bandya2003 <61108642+bandya2003@users.noreply.github.com>
2021-10-14 12:58:13 +0530

    Wrote a program to verify Palindrome
    If we reverse a word and it becomes same as the previous word, it is called palindrome. for eg: Madam

---
+++ JS/palindrome.js
@@ -0,0 +1,13 @@
+function isPalindrome(str){
+  var i, len = str.length;
+  for(i =0; i<len/2; i++){
+    if (str[i]!== str[len -1 -i])
+       return false;
+  }
+  return true;
+}
+
+> isPalindrome('madam')
+  = true
+> isPalindrome('toyota')
+  = false
