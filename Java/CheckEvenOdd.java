b78bccf9c4c1cf95e5cc811f1f1738d672a47cad
Your Name <whiteseeker2000@gmail.com>
2021-10-14 12:52:37 +0530

    added even odd java program

---
+++ Java/CheckEvenOdd.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class CheckEvenOdd
+{
+  public static void main(String args[])
+  {
+    int num;
+    System.out.println("Enter an Integer number:");
+
+    Scanner input = new Scanner(System.in);
+    num = input.nextInt();
+
+    if ( num % 2 == 0 )
+        System.out.println("Entered number is even");
+     else
+        System.out.println("Entered number is odd");
+  }
+}
