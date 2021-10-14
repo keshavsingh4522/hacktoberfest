3da9a35de66a7d8d63c21d3b6e4fe97e65f6dec3
Anand prabhakar <anandprabhakar0507@gmail.com>
2021-10-14 16:57:54 +0530

    update
    added total no of days after birth

--- Java/AgeCounter.java
+++ Java/AgeCounter.java
@@ -26,10 +26,12 @@ public class AgeCounter {
         LocalDate bdate = LocalDate.of(year,month,day);
         
         int age = Period.between(bdate , pres).getYears();
-        
+        Period totalday = Period.of(year, month, day);
         System.out.println(pres);
         System.out.println(bdate);
         System.out.println(age);
+        System.out.println(totalday);
+	System.out.println("total number of days you lived ",totalday.getYears()+totalday.getMonths()+totalday.getDays());
     }
     
 }
