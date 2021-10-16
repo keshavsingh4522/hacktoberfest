0ded38292b5b8f1c70f9637d4620f777d5a80376
shimla1501 <91822830+shimla1501@users.noreply.github.com>
2021-10-16 23:11:35 +0530

    Add files via upload

--- Python/factorial.py
+++ Python/factorial.py
@@ -1,19 +1,8 @@
-# Python program to find the factorial of a number provided by the user.
 
-# change the value for a different result
-num = 7
+x,factorial = int(input("Enter a number: ")),1
+for n in range(x):
+    factorial *= (n+1)
+print("Factorial of",x,"is",str(factorial))
 
-# To take input from the user
-#num = int(input("Enter a number: "))
 
-factorial = 1
 
-# check if the number is negative, positive or zero
-if num < 0:
-   print("Sorry, factorial does not exist for negative numbers")
-elif num == 0:
-   print("The factorial of 0 is 1")
-else:
-   for i in range(1,num + 1):
-       factorial = factorial*i
-   print("The factorial of",num,"is",factorial)
