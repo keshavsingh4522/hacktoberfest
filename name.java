5619504ce549edb39ff4fc8417d3cf2e59f387c0
Suyash Shukla <75569185+suyashshukla30@users.noreply.github.com>
2021-10-16 19:13:04 +0530

    name.java

---
+++ name.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class details{
+public static void main(String args[])
+{
+  String name="Suyash Shukla";
+  String Url="https://github.com/suyashshukla30";
+  System.out.println(name+"  "+Url);
+}}
