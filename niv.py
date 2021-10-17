f0c12ff85e2122b6fa02bbf944ee0012210a13d5
nivedt <92209836+nivedt@users.noreply.github.com>
2021-10-17 19:56:24 +0530

    Create niv.py

---
+++ niv.py
@@ -0,0 +1,8 @@
+#1. Create a greeting for your program.
+print("Welcometo the band name generaator.")
+#2. Ask the user for the city that they grew up in.
+city  = input("What's the name of your city?\n")
+#3. Ask the user for the name of a pet.
+pet = input("What's the name of your pet?\n")
+#4. Combine the name of their city and pet and show them their band name.
+print("Your band name could be " + city + " " + pet)
