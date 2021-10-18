3ca7f4e19b9980b82d7458cddfe2ef29d788f86e
SakshamJain7 <51989243+SakshamJain7@users.noreply.github.com>
2021-10-18 17:41:52 +0530

    Added a python code on Tower of Hanoi puzzle

---
+++ Python/TowerOfHanoi.py
@@ -0,0 +1,13 @@
+# Function to solve this puzzle
+def TowerOfHanoi(n, source, destination, auxiliary):
+    if n == 1:
+        print("Move disk 1 from source", source, "to destination", destination)
+        return
+    TowerOfHanoi(n - 1, source, auxiliary, destination)
+    print("Move disk", n, "from source", source, "to destination", destination)
+    TowerOfHanoi(n - 1, auxiliary, destination, source)
+
+
+# Driver code
+n = 4                               
+TowerOfHanoi(n, 'A', 'B', 'C')        # function calling with arguments
