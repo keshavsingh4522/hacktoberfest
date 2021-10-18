908a87d7ed348ccad7839fecc623041098a65066
Aditya Chakraborty <79187861+aditya-chakraborty@users.noreply.github.com>
2021-10-18 16:22:40 +0530

    wrote more readable code
    Wrote more conventional JavaScript code, improving the readability of the code.

--- JS/AllPrime.js
+++ JS/AllPrime.js
@@ -1,26 +1,22 @@
 let args = process.argv;
 let n = parseInt(args[2])
 
-
-
 for (let i = 1; i <= n; i++){
-    let isPrime = IsPrime(i)
-    if (isPrime == true) {
-        console.log(i)
+    
+    if(isPrime(i)) {
+        console.log(i);
     }
-   
 }
-function IsPrime(x) {
-    let Prime = true
-    for (let div = 2; div * div <= x; div++) {
-       if (x%div == 0) {
-           Prime = false
-           break
-            }
-            else{
-                Prime = true
-            }
 
+function isPrime(num) {
+    let prime = true
+    for (let div = 2; div * div <= num; div++) {
+       if (num % div === 0) {
+           prime = false;
+           break;
+       } else {
+           prime = true;
+       }
     }
-    return Prime
+    return prime;
 }
