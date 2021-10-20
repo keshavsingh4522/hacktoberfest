a3cb3e269ab85223c2e68bff8e9dd281c977ef18
JuliSavalia <92855034+JuliSavalia@users.noreply.github.com>
2021-10-20 15:50:59 +0530

    update comment
    adding a new type of comment

--- CPP/Comment.cpp
+++ CPP/Comment.cpp
@@ -5,5 +5,11 @@ int main(){
     //This is single line comment.
     /*This is multi
     line comment*/
+  
+        """
+      This is a comment
+      written in
+      more than just one line
+      """
    cout<<"Shri Ram"<<endl;
 }
