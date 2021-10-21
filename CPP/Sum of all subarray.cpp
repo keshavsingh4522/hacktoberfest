98ef064f1d75a4dc1e0ea01d5dc26a71eeb4932e
Tamoghna2001 <64722515+Tamoghna2001@users.noreply.github.com>
2021-10-22 02:26:30 +0530

    Added Sum of all subarray.cpp

---
+++ CPP/Sum of all subarray.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        int sum = 0;
+        for (int j = i; j < n; j++)
+        {
+            sum += arr[j];
+            cout << sum << "  ";
+        }
+    }
+}
