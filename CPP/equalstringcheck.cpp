0a9285d7ee9f15fb72f3ebfd96560dc356e01650
Jocelyn <28810573+Noodulz@users.noreply.github.com>
2021-10-25 00:37:12 -0400

    Create equalstringcheck.cpp

---
+++ CPP/equalstringcheck.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	string test = "Hey";
+	string test2 = "Hey";
+
+	if (test[0] == test2[0]) {
+		cout << "True!" << endl;
+	}
+
+	return 0;
+}
