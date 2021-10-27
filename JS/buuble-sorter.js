4f0286d8dffd62e23e9b90d880d56d34f15c443e
staslopatiev <71851826+staslopatiev@users.noreply.github.com>
2021-10-27 17:57:39 +0300

    Add bubble sorter
    add javascript implemenation of bubble sorter

---
+++ JS/buuble-sorter.js
@@ -0,0 +1,16 @@
+const bubbleSorter = (inputArr) => {
+    const len = inputArr.length;
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len; j++) {
+            if (inputArr[j] > inputArr[j + 1]) {
+                let tmp = inputArr[j];
+                inputArr[j] = inputArr[j + 1];
+                inputArr[j + 1] = tmp;
+            }
+        }
+    }
+  
+    return inputArr;
+};
+
+module.exports = bubbleSorter;
