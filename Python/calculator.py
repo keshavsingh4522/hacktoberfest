4bb548ca11f9db8db8443f960f5367309b9a6825
Pacatt <78765627+Pacatt@users.noreply.github.com>
2021-10-27 18:49:34 -0500

    modulus operand added, repeat calculation fixed
    Modulus operation added and repeat calculation now works when "yes" is input.

--- Python/calculator.py
+++ Python/calculator.py
@@ -16,19 +16,26 @@ def multiply(x, y):
 def divide(x, y):
     return x / y
 
+# This function finds the remainder of two numbers
+def mod(x,y):
+    if (y % x) == 0:
+        return "True"
+    else:
+        return "False"
 
 print("Select operation.")
 print("1.Add")
 print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
+print("5.Remainder")
 
 while True:
     # take input from the user
-    choice = input("Enter choice(1/2/3/4): ")
+    choice = input("Enter choice(1/2/3/4/5): ")
 
     # check if choice is one of the four options
-    if choice in ('1', '2', '3', '4'):
+    if choice in ('1', '2', '3', '4', '5'):
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
 
@@ -44,11 +51,19 @@ while True:
         elif choice == '4':
             print(num1, "/", num2, "=", divide(num1, num2))
         
+        elif choice == '5':
+            if mod(num1, num2) == "True":
+                print(num1, " evenly divides into ", num2)
+            else:
+                print(num1, "does not evenly divide into ", num2)
+        
         # check if user wants another calculation
         # break the while loop if answer is no
         next_calculation = input("Let's do next calculation? (yes/no): ")
         if next_calculation == "no":
           break
+        elif next_calculation == "yes":
+            continue
     
     else:
         print("Invalid Input")
