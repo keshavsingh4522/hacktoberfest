dde222213cfae5ca977b0e268f7a66d88cfcf074
Vicky-Raghuwanshi <67050782+Vicky-Raghuwanshi@users.noreply.github.com>
2021-10-28 23:31:09 +0530

    Updated ListElement.py

--- Python/ListElements.py
+++ Python/ListElements.py
@@ -5,27 +5,21 @@ For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 Bonus: Can you do this in one pass?"""
 
 
+lst = list(map(int,input().split()))   ## taking N length of values from user
+k = int(input())   					   ## taking the K value to check the add up of two number from the number
 
-x=int(input('Enter the list size:'))
-l1=[]
-i=0
+result = False
 
-print('Enter list elements:\n')
-for i in range(x):
-    l1.append(int(input()))
-       
-k=int(input('Enter the no:'))
+for i in range(len(lst)):
+	for j in range(1,len(lst)):
+		
+		if lst[i]+lst[j]==k:          ## this condition will check if the add up of two number is equal of K or not
+			result = True			  ## if condition get True then we update result value to True and break the 2nd for loop
+			break
+	if result:						  ## this will break the 1st loop if result == True
+		break
 
-y='False'
-
-start=l1[0]
-
-j=1
-
-for j in range(x):
-    if((start+l1[j])==k):
-        y='True'
-    j+=1
-    
-print(y)
-    
+if result:
+	print(result)
+else:
+	print(result)
