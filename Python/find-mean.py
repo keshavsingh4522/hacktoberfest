1873e2d1831a014c3781950378e21dd45f3a8ada
zakiul fikri <59876777+zakiyul@users.noreply.github.com>
2021-10-28 08:51:28 +0800

    Create find-mean.py

---
+++ Python/find-mean.py
@@ -0,0 +1,11 @@
+def findMean(num):
+    n       = len(num)
+    sum_num  = sum(num)
+    mean    = sum_num / n
+    return mean
+
+data_2017 = [31250,25000,17050,17050,16000,14050,11150,9500,4750,4550]
+
+print(findMean(data_thn_2017))
+
+#output = 15035.0
