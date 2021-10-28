94c3b3c1b507152de6de595365d450028f8c2470
shauniop <skolla3@uic.edu>
2021-10-28 14:19:35 -0500

    Updated with non hardcoded face and eye cascade

--- Python/Face Detection - OpenCV/F_D_Video_Final.py
+++ Python/Face Detection - OpenCV/F_D_Video_Final.py
@@ -1,6 +1,6 @@
 import cv2
-face_cascade=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-eye_cascade=cv2.CascadeClassifier('haarcascade_eye.xml')
+face_cascade=cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+eye_cascade=cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
 
 cap = cv2.VideoCapture(0)   
 while 1: 
