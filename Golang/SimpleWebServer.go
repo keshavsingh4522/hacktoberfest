982bbc2e6882ed8bddb35f8ddac6b41bc8b962f8
Surya Dev Singh <46717624+suryadevsingh@users.noreply.github.com>
2021-10-30 12:17:41 +0530

    Creating A Simple Web Server With Golang
    Creating A Simple Web Server With Golang

---
+++ Golang/SimpleWebServer.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+    "fmt"
+    "html"
+    "log"
+    "net/http"
+)
+
+func main() {
+
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+    })
+
+    http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request){
+        fmt.Fprintf(w, "Hi")
+    })
+
+    log.Fatal(http.ListenAndServe(":8081", nil))
+
+}
