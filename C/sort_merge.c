fc8a8706704d05f0d204efed95bfe2f51e5d6d47
jeel2410 <83400138+jeel2410@users.noreply.github.com>
2021-10-30 09:03:10 +0530

    Create sort_merge.c
    I want to contribute this please assign

---
+++ C/sort_merge.c
@@ -0,0 +1,90 @@
+#include <stdio.h>  
+  
+/* Function to merge the subarrays of a[] */  
+void merge(int a[], int beg, int mid, int end)    
+{    
+    int i, j, k;  
+    int n1 = mid - beg + 1;    
+    int n2 = end - mid;    
+      
+    int LeftArray[n1], RightArray[n2]; //temporary arrays  
+      
+    /* copy data to temp arrays */  
+    for (int i = 0; i < n1; i++)    
+    LeftArray[i] = a[beg + i];    
+    for (int j = 0; j < n2; j++)    
+    RightArray[j] = a[mid + 1 + j];    
+      
+    i = 0; /* initial index of first sub-array */  
+    j = 0; /* initial index of second sub-array */   
+    k = beg;  /* initial index of merged sub-array */  
+      
+    while (i < n1 && j < n2)    
+    {    
+        if(LeftArray[i] <= RightArray[j])    
+        {    
+            a[k] = LeftArray[i];    
+            i++;    
+        }    
+        else    
+        {    
+            a[k] = RightArray[j];    
+            j++;    
+        }    
+        k++;    
+    }    
+    while (i<n1)    
+    {    
+        a[k] = LeftArray[i];    
+        i++;    
+        k++;    
+    }    
+      
+    while (j<n2)    
+    {    
+        a[k] = RightArray[j];    
+        j++;    
+        k++;    
+    }    
+}    
+  
+void mergeSort(int a[], int beg, int end)  
+{  
+    if (beg < end)   
+    {  
+        int mid = (beg + end) / 2;  
+        mergeSort(a, beg, mid);  
+        mergeSort(a, mid + 1, end);  
+        merge(a, beg, mid, end);  
+    }  
+}  
+  
+/* Function to print the array */  
+void printArray(int a[], int n)  
+{  
+    int i;  
+    for (i = 0; i < n; i++)  
+        printf("%d ", a[i]);  
+    printf("\n");  
+}  
+  
+int main()  
+{  
+    int n,i;
+    int a[100];
+    printf("enter number of elements (max. 100) \n");
+    scanf("%d",&n);
+   
+   for(i=0;i<n;i++)
+   {
+       printf("enter elements [%d] ",i+1);
+       scanf("%d",&a[i]);
+   }
+   
+    printf("Before sorting array elements are - \n");  
+    printArray(a, n);  
+    mergeSort(a, 0, n - 1);  
+    printf("After sorting array elements are - \n");  
+    printArray(a, n);  
+    return 0;  
+}  
