bbe67b8c7813a4de2efe622443bf7b4ae4cfa005
Ran Crump <rancrump18@gmail.com>
2021-10-30 12:54:58 -0500

    Added Bubble Sort Kotlin

---
+++ Kotlin/BubbleSort.kt
@@ -0,0 +1,26 @@
+/**
+ * You can edit, run, and share this code. 
+ * play.kotlinlang.org 
+ */
+
+fun bubbleSort(arr:IntArray):IntArray{
+    var swap = true
+    while(swap){
+        swap = false
+        for(i in 0 until arr.size-1){
+            if(arr[i] > arr[i+1]){
+                val temp = arr[i]
+                arr[i] = arr[i+1]
+                arr[i + 1] = temp
+
+                swap = true
+            }
+        }
+    }
+    return arr
+}
+
+fun main(args: Array<String>) {
+    val list = bubbleSort(intArrayOf(4,1,-3,0,8,2,-1))
+    for (k in list) print("$k ")
+}
