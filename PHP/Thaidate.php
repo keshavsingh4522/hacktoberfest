169e7aa01e632112788b63b7c0e4c0e8b76ac647
sankhumpha <sankhumpha84@gmail.com>
2021-10-31 09:08:35 +0700

    add php function thaidate

---
+++ PHP/Thaidate.php
@@ -0,0 +1,14 @@
+<?php 
+function thaidate($strDate) {
+    $strYear = date("Y",strtotime($strDate))+543;
+    $strMonth= date("n",strtotime($strDate));
+    $strDay= date("j",strtotime($strDate));
+    $strHour= date("H",strtotime($strDate));
+    $strMinute= date("i",strtotime($strDate));
+    $strSeconds= date("s",strtotime($strDate));
+    $strMonthCut = Array("","ม.ค.","ก.พ.","มี.ค.","เม.ย.","พ.ค.","มิ.ย.","ก.ค.","ส.ค.","ก.ย.","ต.ค.","พ.ย.","ธ.ค.");
+    $strMonthThai=$strMonthCut[$strMonth];
+    return "$strDay $strMonthThai $strYear";
+}
+
+?>
