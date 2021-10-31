ba05de02ae1a18d6a37ccf2c335345be5ff37adb
Wendell00 <62032363+Wendell00@users.noreply.github.com>
2021-10-31 11:13:41 -0300

    Create playMusic.py

---
+++ Python/playMusic.py
@@ -0,0 +1,13 @@
+from playsound import playsound
+playsound('C:/Users/WD/Desktop/hylander.mp3') # Your music directory
+
+# You need to install the playsound library
+# Use    pip install playsound    in terminal
+# Wait to complete install and start your code
+
+
+# Examples:
+# C:/Users/WD/Desktop/Young thug.mp3
+# C:/Users/WD/Documents/holdon.wav
+
+# WD is my user, your user will be different
