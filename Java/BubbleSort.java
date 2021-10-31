92aedf7ea4729c8d305dde45d4caf8bb4fd7574c
ajaycode <ajaycode@users.noreply.github.com>
2021-10-30 23:06:54 -0700

    Update BubbleSort.java

--- Java/BubbleSort.java
+++ Java/BubbleSort.java
@@ -16,7 +16,7 @@ public class BubbleSortExample {
   
     }  
     public static void main(String[] args) {  
-                int arr[] ={3,60,35,2,45,320,5};  
+                int arr[] ={3,60,35,2,45,320,5,64,97};  
                  
                 System.out.println("Array Before Bubble Sort");  
                 for(int i=0; i < arr.length; i++){  
