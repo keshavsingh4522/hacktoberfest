b46eef32986444b503f2b8a70207c1c481998550
Rishabh Agrawal <rishabhagrawal2020@gmail.com>
2022-10-01 00:03:47 +0530

    added binary tree inorder traversal program

---
+++ CPP/binary-tree-inorder-traversal.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> vec;
+    vector<int> inorderTraversal(TreeNode* root) {
+        if(root==NULL){
+            return vec;
+        }else{
+            inorderTraversal(root->left);
+            vec.push_back(root->val);
+            inorderTraversal(root->right);
+        }
+        return vec;
+    }
+};
