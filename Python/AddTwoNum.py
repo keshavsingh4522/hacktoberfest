5285052afdcad82ee89a5fca73ec1ffa35a23270
Raval Jinit <72157334+RavalJinit@users.noreply.github.com>
2022-10-01 17:16:22 +0530

    Updating the code
    creating a simple code

--- Python/AddTwoNum.py
+++ Python/AddTwoNum.py
@@ -1,17 +1,6 @@
 # just change Add to sum two numbers wether in float or int
-num1 = 15.0
-num2 = 12.0
+num1 = input("Enter num1 : ")
+num2 = input("Enter num2 : ")
 
-# change Add to sum 
-sum = num1 + num2 
-
-#you can truncate or set upto which decimal place you want output
-example:{
-float = 2.154327
-format_float = "{:.2f}".format(float)
-print(format_float)}
-
-#else just print sum as mentioned below
-
-print("Sum of {0} and {1} is {2}" .format(num1, num2, sum)) 
-
+sum = int(num1) + int(num2)
+print(sum)
