9e80faf5008c6dfe51c32248d88ca09c3f4f443c
Bhavik Prajapati <109121443+Bhavik-prajapati@users.noreply.github.com>
2022-10-21 16:09:00 +0530

    Create RevArrayUsingTwopointer.cpp

---
+++ CPP/RevArrayUsingTwopointer.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+void reverseArray(int *arr,int n){
+
+   int s=0;
+   int e=n-1;
+
+   while (s<=e)
+   {
+      swap(arr[s],arr[e]);
+      s++;e--;
+   }
+
+}
+int main(){
+
+   int arr[]={1,2,3,4,5,6};
+   int n=6;
+   reverseArray(arr,n);
+   for (int i = 0; i < n; i++)
+   {
+      cout<<arr[i]<<" ";
+   }
+}
