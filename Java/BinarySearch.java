9565642bf6bedd6fbcc1255ba88ac3c75b7bc5ab
petwal-tanishka <104723597+petwal-tanishka@users.noreply.github.com>
2022-10-26 04:52:09 +0900

    Update BinarySearch.java
    Updated line 9 will decrease the time complexity.

--- Java/BinarySearch.java
+++ Java/BinarySearch.java
@@ -2,12 +2,11 @@ import java.util.*;
 public class BinarySearch { 
 
     public static int binarySearch(int[] arr, int x) {
-    	//Your code goes here
         int start = 0;
         int end  = arr.length-1;
         
         while(start <= end){
-            int mid = (start+end)/2;
+            int mid = start+(end-start)/2;
             if(arr[mid] == x){
                 return mid;
             }else if(arr[mid] > x){
