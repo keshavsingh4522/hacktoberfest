e3a0a9554141d819b12e255821e3cad1131ac2f6
Soubhik1999 <116966498+Soubhik1999@users.noreply.github.com>
2022-10-29 23:39:14 +0530

    Create Array.java

---
+++ Array.java
@@ -0,0 +1,10 @@
+public class Array {
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+
+        for (int element: array) {
+            System.out.println(element);
+        }
+    }
+}
