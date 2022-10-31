c0d0397e4e1ffdae110fcf58c11d678162f027b5
Pongsak J <PongsakDev@users.noreply.github.com>
2022-10-31 21:15:03 +0700

    Create file "Loops.dart"
    Add Ex Label with Break Loops DART program

---
+++ DART/Loops.dart
@@ -0,0 +1,44 @@
+// Ex Label with Break
+//
+
+void main() { 
+   outerloop: // This is the label name 
+   
+   for (var i = 0; i < 5; i++) { 
+      print("Innerloop: ${i}"); 
+      innerloop: 
+      
+      for (var j = 0; j < 5; j++) { 
+         if (j > 3 ) break ; 
+         
+         // Quit the innermost loop 
+         if (i == 2) break innerloop; 
+         
+         // Do the same thing 
+         if (i == 4) break outerloop; 
+         
+         // Quit the outer loop 
+         print("Innerloop: ${j}"); 
+      } 
+   } 
+}
+
+// output---->
+
+// Innerloop: 0
+// Innerloop: 0
+// Innerloop: 1
+// Innerloop: 2
+// Innerloop: 3
+// Innerloop: 1
+// Innerloop: 0
+// Innerloop: 1
+// Innerloop: 2
+// Innerloop: 3
+// Innerloop: 2
+// Innerloop: 3
+// Innerloop: 0
+// Innerloop: 1
+// Innerloop: 2
+// Innerloop: 3
+// Innerloop: 4
