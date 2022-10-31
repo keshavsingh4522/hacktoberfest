9fe48163ff7167e93c7e8f7f1ab9d2dd3d353af8
ReeshuVishall <117099617+ReeshuVishall@users.noreply.github.com>
2022-10-31 21:03:28 +0530

    Create count.py

---
+++ count.py
@@ -0,0 +1,6 @@
+message = 'python is popular programming language'
+
+# number of occurrence of 'p'
+print('Number of occurrence of p:', message.count('p'))
+
+# Output: Number of occurrence of p: 4
