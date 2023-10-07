30b01edeebdb9452c62075ea550de000cf6592c1
Joyel Johny <81413791+JoyelJohny@users.noreply.github.com>
2023-10-07 20:42:32 +0530

    Update calculator.py
    Added Square function to the program

--- Python/calculator.py
+++ Python/calculator.py
@@ -15,19 +15,24 @@ def multiply(x, y):
 # This function divides two numbers
 def divide(x, y):
     return x / y
-
+    
+# This function squares a number
+def square(x):
+    return x * x
 
 print("Select operation.")
 print("1.Add")
 print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
+print("5.Square")
 
 while True:
     # take input from the user
-    choice = input("Enter choice(1/2/3/4): ")
+    choice = input("Enter choice(1/2/3/4/5): ")
 
     # check if choice is one of the four options
+    
     if choice in ('1', '2', '3', '4'):
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
@@ -43,12 +48,18 @@ while True:
 
         elif choice == '4':
             print(num1, "/", num2, "=", divide(num1, num2))
+
         
         # check if user wants another calculation
         # break the while loop if answer is no
         next_calculation = input("Let's do next calculation? (yes/no): ")
         if next_calculation == "no":
           break
-    
+    elif choice == '5':
+        num1 = float(input("Enter the number: "))
+        print(num1,"*",num1,"=",square(num1))
+        next_calculation = input("Let's do next calculation? (yes/no): ")
+        if next_calculation == "no":
+          break
     else:
         print("Invalid Input")
