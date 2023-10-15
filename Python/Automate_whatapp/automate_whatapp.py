0bad583c7bab44bdbcd02ae50dbef2a497501d5b
Mohd Kaif <107213582+Kaif9999@users.noreply.github.com>
2023-10-15 20:15:00 +0530

    Update automate_whatapp.py
    i have added a feature so that user can define delay between each messages send

--- Python/Automate_whatapp/automate_whatapp.py
+++ Python/Automate_whatapp/automate_whatapp.py
@@ -1,11 +1,19 @@
 import time
 import pyautogui
 
-def send():
-  time.sleep(10)
-  text=open('message.txt')
-  for each_line in text:
-    pyautogui.typewrite(each_line)
-    pyautogui.press('enter')
+def send_message_with_delay(delay):
+    time.sleep(delay)
+    try:
+        with open('message.txt', 'r') as text:
+            for each_line in text:
+                pyautogui.typewrite(each_line)
+                pyautogui.press('enter')
+                print(f"Sent: {each_line.strip()}")
+    except FileNotFoundError:
+        print("File 'message.txt' not found. Make sure the file exists in the current directory.")
+    finally:
+        text.close()
 
-send()
+# Get user input for the delay
+delay = int(input("Enter the delay time (in seconds): "))
+send_message_with_delay(delay)
