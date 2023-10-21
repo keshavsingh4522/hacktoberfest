52a2262331bb9225b9ae3cff57ad2954485be44b
Vikash <114097162+ivikashgorai@users.noreply.github.com>
2023-10-22 03:52:16 +0530

    Update BinarySearch.java
    1) I changed "mid = (start + end) / 2" to "mid = start + (end - start) / 2" to ensure that the calculation of the middle index doesn't exceed the bounds of the integer range.
    
    2)I modified the code to handle both ascending and descending order arrays and made slight adjustments to the return statement.

--- Java/BinarySearch.java
+++ Java/BinarySearch.java
@@ -1,21 +1,42 @@
 import java.util.*;
 public class BinarySearch { 
-
-    public static int binarySearch(int[] arr, int x) {
+    
+ public static int binarySearch(int[] arr, int target) {
     	//Your code goes here
         int start = 0;
         int end  = arr.length-1;
-        
+
+        if(arr[0]<arr[arr.length-1]){ // if array is sorted in ascending order
         while(start <= end){
-            int mid = (start+end)/2;
-            if(arr[mid] == x){
-                return mid;
-            }else if(arr[mid] > x){
-                end = (mid-1);
-            }else {
+            int mid = start + (end-start)/2; 
+            if(arr[mid] < target){
                 start = (mid+1);
+            }
+            else if(arr[mid] > target){
+                end = (mid-1);
+            }
+            else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+     else{ // if array is sorted in descending order
+        while(start <= end){
+            int mid = start + (end-start)/2;
+            if(arr[mid] < target){
+                end = (mid-1);
+            }
+            else if(arr[mid] > target){
+                start = (mid+1);
+            }
+            else {
+                return mid;
             }
         }
         return -1;
     }
 }
+}
+
