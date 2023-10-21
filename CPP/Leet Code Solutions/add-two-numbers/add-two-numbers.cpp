8cd2e65ede41b4c4660a13d486ae2dbaaa87f211
Shivank Kumar <shivankkumar479@gmail.com>
2023-10-21 22:46:17 +0530

    new changes

--- CPP/Leet Code Solutions/add-two-numbers/add-two-numbers.cpp
+++ CPP/Leet Code Solutions/add-two-numbers/add-two-numbers.cpp
@@ -26,3 +26,12 @@ public:
         return preNode.next;
     }
 };
+
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cin>> a >> b;
+    cout << a+b << endl;
+    return 0;
+}
