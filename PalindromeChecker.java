3adf6e8e331c448ca488eff4d5bbc85ce5c2508e
Ashima1503 <78199579+Ashima1503@users.noreply.github.com>
2023-10-28 18:50:02 +0530

    Create PalindromeChecker.java

---
+++ PalindromeChecker.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class PalindromeChecker {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a word or phrase: ");
+        String input = scanner.nextLine();
+        scanner.close();
+
+        if (isPalindrome(input)) {
+            System.out.println(input + " is a palindrome.");
+        } else {
+            System.out.println(input + " is not a palindrome.");
+        }
+    }
+
+    public static boolean isPalindrome(String str) {
+        // Remove spaces and convert to lowercase for case-insensitive comparison
+        str = str.replaceAll("\\s", "").toLowerCase();
+
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
