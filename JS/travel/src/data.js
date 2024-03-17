2d623ac132fefb11a711aee85b0d2dde9b4c5bcc
nadasuhailAyesh12 <missnadaayesh@gmail.com>
2024-03-17 13:47:20 +0200

    adding-my-contribute

---
+++ JS/travel/src/data.js
@@ -0,0 +1,24 @@
+export const data=[
+    {
+        country:"JAPAN",
+        imgSource:"https://source.unsplash.com/WLxQvbMyfas",
+        parag:"Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        placeName:"Mount Fuji",
+        date:"12 Jan, 2021 - 24 Jan, 2021"
+    },
+    {
+        country:"AUSTRALIA",
+        imgSource:"https://source.unsplash.com/JmuyB_LibRo",
+        parag:"The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings.",
+        placeName:"Sydney Opera House",
+        date:"27 May, 2021 - 8 Jun, 2021"
+    },
+    {
+        country:"NORWAY",
+        imgSource:"https://source.unsplash.com/3PeSjpLVtLg",
+        parag:"The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
+        placeName:"Geirangerfjord",
+        date:"01 Oct, 2021 - 18 Nov, 2021"
+    },
+
+]
